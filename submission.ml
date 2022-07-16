323dd223ab18d0b5a778f079420172cb1c71c4f1
powcoder代写 <powcoder@163.com>
2022-07-16 16:32:24 +0800

    程序代写 加微信 powcoder Programming Help Add Wechat powcoder

---
+++ submission.ml
@@ -0,0 +1,23 @@
+
+(** Problem **)
+
+module StringMap = Map.Make(String)
+
+type sinstr =
+| SLit of int
+| SVar of string
+| SPlus 
+| SMinus 
+| SMult
+| SDiv
+
+let prog_exec_instr (symbol_table: int StringMap.t) (inst : sinstr) (stack : int list) 
+  : (int list) option = 
+  (** YOUR CODE HERE **)
+  None
+
+let prog_exec (symbol_table: int StringMap.t) (prog : sinstr list) : int option= 
+  (** YOUR CODE HERE **)
+  None
+
+
